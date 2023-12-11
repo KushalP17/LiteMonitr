ffed420517359867a94b5a23cbe9f7b52acf8cc6
Kushal Parekh <98786169+KushalP17@users.noreply.github.com>
2023-12-11 15:37:09 -0500

    Built-in Option Assets, Conversion Python Script
    Added 64x32 Sprites, and the python tool used to convert them from PNGs to C arrays

---
+++ testfoldersprites/pngtocarrayrgb.py
@@ -0,0 +1,26 @@
+import os, sys
+from PIL import Image
+
+path = "C://Users//parek//Desktop//testfoldersprites"
+
+f = open(path + "//header.h", "w")
+
+for File in os.listdir(path):
+    try:
+        im = Image.open(path + "//" + File, 'r')
+        pix = im.load()
+        f.write("//" + File + " " + str(im.height) + "x" + str(im.width) + "px\n")
+        f.write("const uint8_t " + File[0:-4] + "[" + str(im.height) + "][" + str(im.width) + "][" + str(3) + "]= {\n")
+        idx = 0
+        for hidx in range(im.height):
+            for widx in range(im.width):
+                if idx == 0:
+                    f.write(str(pix[widx, hidx][0]) + ", " + str(pix[widx, hidx][1]) + ", " + str(pix[widx, hidx][2]))
+                else:
+                    f.write(", " + str(pix[widx, hidx][0]) + ", " + str(pix[widx, hidx][1]) + ", " + str(pix[widx, hidx][2]))
+                # print(pix[widx, hidx])
+                idx += 1
+
+        f.write("\n};\n\n")
+    except OSError:
+        pass
